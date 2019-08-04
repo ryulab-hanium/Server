b315ba53ec21baf7832f01d3700a39de786f8433
dyeonss <ehdus7310@gmail.com>
2019-08-04 18:18:08 +0900

    Update ParkingInfoController.java

--- src/main/java/com/seoulparking/controller/ParkingInfoController.java
+++ src/main/java/com/seoulparking/controller/ParkingInfoController.java
@@ -1,6 +1,5 @@
 package com.seoulparking.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,30 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.seoulparking.dao.ParkingInfoDao;
 import com.seoulparking.dto.ParkingInfoDto;
 
-@SuppressWarnings("hiding")
 @RestController
-public class ParkingInfoController<ParkingInfoDto> {
+public class ParkingInfoController{
 
 	@Autowired
 	private ParkingInfoDao infodao;
 
 	@RequestMapping(value = "/pname")
-	public List<ParkingInfoDto> findByPname(String pname ) throws Exception {
-		return (List<ParkingInfoDto>) infodao.findByPname("리츠칼튼호텔");  
+	public List<ParkingInfoDto> findByPname(String pname) throws Exception {
+		return infodao.findByPname("리츠칼튼호텔");  
 	}
 	
 	@RequestMapping(value = "/all")
 	public List<ParkingInfoDto> findAll() throws Exception {
-		List<ParkingInfoDto> result = new ArrayList<ParkingInfoDto>();
-		result= (List<ParkingInfoDto>) infodao.findAll();
-		
-		for(ParkingInfoDto rs :result)
-			System.out.println("출력:"+rs);
-		return result;
+		return infodao.findAll();
 	}
 	
 	@RequestMapping(value = "/{addr2}")
-	public List<ParkingInfoDto> findByAddr2(@PathVariable String addr2) throws Exception {
-		return (List<ParkingInfoDto>) infodao.findByAddr2(addr2); 
+	public List<ParkingInfoDto> findByAddr2(@PathVariable("addr2") String addr2) throws Exception {
+		return infodao.findByAddr2(addr2); 
 	}
 }
