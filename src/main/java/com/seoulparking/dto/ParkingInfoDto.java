3cf5eedb3acf5e4077b1b75c3d5f6a1a6eec42ef
dyeonss <ehdus7310@gmail.com>
2019-08-04 18:19:05 +0900

    Update ParkingInfoDto.java

--- src/main/java/com/seoulparking/dto/ParkingInfoDto.java
+++ src/main/java/com/seoulparking/dto/ParkingInfoDto.java
@@ -4,15 +4,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
 
 @Data
-@Document(collection="parking")  
+@Document(collection="parking_lot")  
 public class ParkingInfoDto {
 
 	private String pname;
-	//private String addr1;  
+	private String addr1;  
 	private String addr2;
 	private String total;
-	//private String avail;
-	//private List<String> rate;
-	//private List<String> time;
+	private String avail;
+	private List<String> rate;
+	private List<String> time;
 }
 
